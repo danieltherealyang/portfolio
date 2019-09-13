a9d559cd59f4e2b16116cfb0c3419f34ee9c2434
Daniel <danielyangkang@gmail.com>
2019-09-13 11:22:22 -0700

    Finished version

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.print('\u000C'); //clear the terminal
+        System.out.println('\u000C'); //clear the terminal
         System.out.println("Welcome to my portfolio");
         System.out.println("Press 0 to quit");
         System.out.println("Press 1 for Monkey jumpers");
@@ -12,11 +12,11 @@ public class Main
 
         Scanner scannerObj = new Scanner(System.in); //create a Scanner Object
         String menuPress = scannerObj.nextLine(); // take user input
-        while (!menuPress.equals("0"))
+        while (!menuPress.equals("0")) //while loop so that 0 returns to menu
         {
             if (menuPress.equals("1")) //run Monkey Jumpers if key press is 1
             {
-                System.out.print('\u000C'); //clear the terminal
+                System.out.println('\u000C'); //clear the terminal
                 System.out.println("Welcome to Monkey jumpers");
                 System.out.println("What jumper do you want?");
                 //take user input to define the jumper 
@@ -28,73 +28,70 @@ public class Main
                 //convert the String input variable to an int variable
                 int numJumper = Integer.parseInt(numJumperString);
             
-                MonkeyJumpers jumperObj = new MonkeyJumpers();
+                MonkeyJumpers jumperObj = new MonkeyJumpers(); //run the Monkey Jumpers class
                 jumperObj.runJumpers(numJumper,jumpers);
             }
             
             if (menuPress.equals("2"))
             {
-                System.out.print('\u000C');
-                Calculator calculate = new Calculator();
-                System.out.println("Press 1 to add");
+                System.out.println('\u000C'); //clear terminal
+                System.out.println("Press 1 to add"); //display menu
                 System.out.println("Press 2 to subtract");
                 System.out.println("Press 3 to multiply");
-                System.out.println("Press 4 to divide");
-                System.out.println("Press 5 for rapid fire mode");
                 System.out.println("Press 0 to exit Calculator");
-                String calcMenu = scannerObj.nextLine();
+                String calcMenu = scannerObj.nextLine(); //take user input as calculator menu var
                                 
-                while (!calcMenu.equals("0"))
+                while (!calcMenu.equals("0")) //while loop to make 0 the escape
                 {
-                    if (calcMenu.equals("1"))
+                    if (calcMenu.equals("1")) //add option
                     {
-                       System.out.print('\u000C');
+                       System.out.println('\u000C');
                        System.out.println("Which numbers do you want to add?");
                        String sumNum = scannerObj.nextLine();
-                       ArrayList<Integer> numList = new ArrayList<>();
-                       while (!sumNum.equals("="))
+                       ArrayList<Integer> numList = new ArrayList<>(); //add user input to Arraylist
+                       while (!sumNum.equals("=")) //make the = sign the escape to terminate
                        {   
                            
-                           int intSumNum = Integer.parseInt(sumNum);
-                           numList.add(intSumNum);
-                           sumNum = scannerObj.nextLine();
-                           if (sumNum.equals("="))
+                           int intSumNum = Integer.parseInt(sumNum); // convert sumNum to int
+                           numList.add(intSumNum); //add the user input to array numList
+                           sumNum = scannerObj.nextLine(); // ask for user input again
+                           if (sumNum.equals("=")) //if user enters = then while loop will terminate
                            {
                                break;
                            }
                        }
                                              
-                       int sum = 0;
-                       for (int i : numList)
+                       int sum = 0; //define sum as 0
+                       for (int i : numList) //loop through aray numList
                        {
-                           sum = sum + i;
+                           sum = sum + i; //add each element of numList together
                        }
                        
-                       System.out.println("Your answer is: " + sum);
-                       System.out.println("Press 0 to exit Calculator");
+                       System.out.println("Your answer is: " + sum); //display answer
+                       System.out.println("Press 0 to exit Calculator"); //exit prompt
                        calcMenu = scannerObj.nextLine();
-                       System.out.print('\u000C');
+                       System.out.println('\u000C');
                     }
                     
-                    if (calcMenu.equals("2"))
+                    if (calcMenu.equals("2")) //subtract option
                     {
-                        System.out.print('\u000C');
-                       System.out.println("Which numbers do you want to subtract?");
-                       String sumNum = scannerObj.nextLine();
-                       ArrayList<Integer> numList = new ArrayList<>();
+                        System.out.println('\u000C');
+                       System.out.println("Which numbers do you want to subtract?"); //prompt
+                       String sumNum = scannerObj.nextLine(); //take user input
+                       ArrayList<Integer> numList = new ArrayList<>(); //user input to array
                        while (!sumNum.equals("="))
                        {   
-                           int intSumNum = Integer.parseInt(sumNum);
-                           numList.add(intSumNum);
-                           sumNum = scannerObj.nextLine();
-                           if (sumNum.equals("="))
+                           int intSumNum = Integer.parseInt(sumNum); //user input to integer
+                           numList.add(intSumNum); //add user input to arraylist
+                           sumNum = scannerObj.nextLine(); 
+                           if (sumNum.equals("=")) //break code
                            {
                                break;
                            }
                        }
                                              
-                       int sum = numList.get(0) + 1;
-                       for (int i : numList)
+                       int sum = numList.get(0) + 1; //first element of numList and add 1, idk why but it works
+                       for (int i : numList) //loop through and subtract
                        {
                            sum = sum - i;
                        }
@@ -102,12 +99,12 @@ public class Main
                        System.out.println("Your answer is: " + sum);
                        System.out.println("Press 0 to exit Calculator");
                        calcMenu = scannerObj.nextLine();
-                       System.out.print('\u000C');
+                       System.out.println('\u000C');
                     }
                     
                     if (calcMenu.equals("3"))
                     {
-                        System.out.print('\u000C');
+                       System.out.println('\u000C');
                        System.out.println("Which numbers do you want to multiply?");
                        String productList = scannerObj.nextLine();
                        ArrayList<Integer> numList = new ArrayList<>();
@@ -126,34 +123,36 @@ public class Main
                        int Product = 0;
                        for (int i : numList)
                        {
-                           Product = Product + i;
+                           Product = Product * i;
                        }
                        
                        System.out.println("Your answer is: " + Product);
                        System.out.println("Press 0 to exit Calculator");
                        calcMenu = scannerObj.nextLine();
-                       System.out.print('\u000C');
+                       System.out.println('\u000C');
                     }
-                    if (calcMenu.equals("4"))
-                    {
-                        
-                    }
-                    if (calcMenu.equals("5"))
-                    {
-                        
-                    }
-                    System.out.print('\u000C');
+                    
+
                 }
+                
             }
             
-            if (menuPress.equals("3"))
+            if (menuPress.equals("3")) //display menu option
             {
-                System.out.print('\u000C'); //clear the terminal
+                System.out.println('\u000C'); //clear the terminal
                 System.out.println("Welcome to my portfolio");
                 System.out.println("Press 0 to quit");
                 System.out.println("Press 1 for Monkey jumpers");
                 System.out.println("Press 2 for Calculator");
+                System.out.println("Press 3 to display menu");
+                menuPress = scannerObj.nextLine();
             }
+            
+            System.out.println('\u000C'); //clear the terminal
+            System.out.println("Welcome to my portfolio");
+            System.out.println("Press 0 to quit");
+            System.out.println("Press 1 for Monkey jumpers");
+            System.out.println("Press 2 for Calculator");
             System.out.println("Press 3 to display menu");
             menuPress = scannerObj.nextLine();
         }
